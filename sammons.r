bd99bba3082daa60a41d80efda63199a32074e03
Nilton Vasques <nilton.vasques@gmail.com>
2016-05-17 09:53:49 -0300

    Add jaccard dist to sammons

--- sammons.r
+++ sammons.r
@@ -2,6 +2,14 @@ library(MASS)
 cosineDist <- function(x){
   as.dist(1 - x%*%t(x)/(sqrt(rowSums(x^2) %*% t(rowSums(x^2))))) 
 }
+jacardDist <- function(x){
+  ab <- x%*%t(x)
+  d1d1 <- rowSums(x^2)
+  #d2d2 <- t(rowSums(x^2))
+  aa <- d1d1 %*% t(rep(1,length(d1d1)))
+  aa <- aa + t(aa)
+  as.dist(1 - ab/(aa - ab)) 
+}
 
 drawCluster <- function(label, norm,  method, frequencys, sam){
   points <- as.data.frame(sam$points)
@@ -10,8 +18,7 @@ drawCluster <- function(label, norm,  method, frequencys, sam){
   if(file.exists(filename)){
     clusters = read.table(filename)
     clusters <- clusters[frequencys$id,] + 1
-    labels <- clusters
-    labels <- sort(unique(labels))
+    labels <- sort(unique(clusters))
     labels <- as.character(paste("crisp",labels))
     n_labels <- length(labels)
     print(labels)
@@ -22,12 +29,13 @@ drawCluster <- function(label, norm,  method, frequencys, sam){
     pdf(file=pdf_path)
 
     print("Plot results")
-    points <- (points-min(points))/(max(points)-min(points))
+    #points <- (points-min(points))/(max(points)-min(points))
     x <- points$V1
     y <- points$V2
-    plot(x, y, col=rainbow(n_labels)[clusters], pch=19)
+    plot(x, y, col=rainbow(max(clusters)+1)[clusters], pch=19)
+    #plot(x, y, col=rainbow(n_labels)[clusters], pch=19)
     title(label)
-    legend('topright', labels, col=rainbow(n_labels), lty=1, bty='n', cex=.75)
+    legend('topright', labels, col=rainbow(max(clusters)+1), lty=1, bty='n', cex=.75)
     dev.off()
 
     # How to plot each cluster separated and respecting fuzzy partitions
@@ -67,6 +75,10 @@ table <- table[!duplicated(table[,c(1:ncol(table)-1)]), ]
 print("Computing all distances with cosine measure")
 d = cosineDist(as.matrix(table))
 applySammonMapping(table, d, "cosine", title)
+
+print("Computing all distances with jaccard measure")
+d = jacardDist(as.matrix(table))
+applySammonMapping(table, d, "jaccard", title)
 
 print("Computing all distances with euclidian measure")
 d = dist(as.matrix(table))
